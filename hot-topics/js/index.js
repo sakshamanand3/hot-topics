e9f64fb8218d3c8134c73b36bd9712aad1c7b066
sakshamanand3 <59570902+sakshamanand3@users.noreply.github.com>
2020-04-18 13:46:38 -0400

    Update index.js

--- hot-topics/js/index.js
+++ hot-topics/js/index.js
@@ -1,92 +1,38 @@
+// FETCHING HTML PARTIAL
+const loadBtn = document.querySelectorAll(".navbar li a");
+const loadHere = document.querySelector(".container");
+let url = "./content/partial-1.html";
 
 
-//getting references to the interactive elements of the page
-const tagline = document.querySelectorAll('.tagline');
-const container = document.querySelector('.container');
-const image = document.querySelectorAll('.image');
-const link = document.querySelector('.nav');
-
-
-//saving the path to initial content into variable:
-let url = "content/partial-1.html";
-
-
-// function that runs when one of the links is clicked
-const loadContent = (event) => {
-
-
-	event.preventDefault();
-
-	let h = event.target.textContent;
-
-	if( h === "Home") {
-
-		let tagline = "home";
-
-		let url = '../content/partial-1.html';
-
-		fetch(url)
-		.then(function (rsp) {
-			return rsp.text();
-		})
-		.then(function (data) {
-			container.innerHTML = data;
-			tagline.innerHTML = tagline;
-			document.getElementById('bgimg').style.backgroundImage = "url('./img/p-mast.png')";
-		});
-
-		.catch(function(error) { console.log(error.message); });
-
-	} else if (h === "Portfolio") {
-
-		let url = '../content/partial-2.html';
-		
-		fetch(url)
-		.then(function (rsp) {
-			return rsp.text();
-
-		}).then(function (data) {
-			container.innerHTML = data;
-			tagline.innerHTML = tagline;
-			document.getElementById('bgimg').style.backgroundImage = "url('./img/mast.png')";
-		})
-
-
-		.catch(function(error) { console.log(error.message); });
-
-	}
-
+function handleClick(e) {
+  e.preventDefault();
+  
+  let clickedLink = e.target;
+  
+  url = clickedLink.href;
+  
+  handleAjaxRequest();
 }
 
-	const selectContent = () => {
-
-			fetch(url)
-		.then(function (rsp) {
-			return rsp.text();
-
-		.then(function (data) {
-			container.innerHTML = data;
-			tagline.innerHTML = tagline;
-			document.getElementById('bgimg').style.backgroundImage = "url('./img/mast.png')";
-
-		}) 
-
-		.catch(function(error) { console.log(error.message); 
-
-		});
-
-	}
+for (let navItem of loadBtn) {
+  navItem.addEventListener("click", handleClick);
 }
 
+function handleAjaxRequest() {
+  fetch(url)
+    .then(function (response) {
+      if (response.statusText === "OK") {
+        return response.text();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(function (data) {
+      loadHere.innerHTML = data;
+    })
+    .catch(function (error) {
+      loadHere.innerHTML = `<pre>${error.name}: ${error.message}</pre>`;
+    });
+};
 
-
-	for (let l of links) {
-		1.addEventListener("click", loadContent)
-	}
-
-
-selectContent();
-
-
-
+handleAjaxRequest();
 
